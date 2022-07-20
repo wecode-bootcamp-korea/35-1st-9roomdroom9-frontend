9c868337270e1f7166c3cea1e99505332a93c520
doll8721 <doll8721@gmail.com>
2022-07-20 15:43:30 +0900

    feature/cart

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import './Cart.scss';
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div className="cart">
+      <div className="title">
+        <span>장바구니</span>
+      </div>
+      <div className="cartWrap">
+        <div className="cartContent">
+          <div className="cartTop">
+            <label>
+              <input type="checkbox" />
+              전체선택
+            </label>
+            <span>선택삭제</span>
+          </div>
+          <div className="product">
+            <ul>
+              <li>배달이친구들 손바닥양면노트 2종</li>
+            </ul>
+          </div>
+        </div>
+        <div className="paymentArea" />
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
