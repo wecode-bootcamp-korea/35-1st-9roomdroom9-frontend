cbf1bfc3ec7b10b7e082b3ca4c7c90fdac3e5cc2
doll8721 <doll8721@gmail.com>
2022-07-21 09:50:26 +0900

    temporary save

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -18,11 +18,61 @@ const Cart = () => {
           </div>
           <div className="product">
             <ul>
-              <li>배달이친구들 손바닥양면노트 2종</li>
+              <li>
+                <input type="checkbox" />
+                <div className="img" />
+                <div className="text">
+                  <span>배달이친구들 손바닥양면노트 2종</span>
+                </div>
+                <div>
+                  <button>-</button>
+                  <input type="number" value="1" />
+                  <button>+</button>
+                </div>
+                <div>
+                  <span>0원</span>
+                  <button>x</button>
+                </div>
+              </li>
+              <li>
+                <input type="checkbox" />
+                <div className="img" />
+                <div className="text">
+                  <span>배달이친구들 손바닥양면노트 2종</span>
+                </div>
+                <div>
+                  <button>-</button>
+                  <input type="number" value="1" />
+                  <button>+</button>
+                </div>
+                <div>
+                  <span>0원</span>
+                  <button>x</button>
+                </div>
+              </li>
             </ul>
           </div>
         </div>
-        <div className="paymentArea" />
+        <div className="paymentArea">
+          <div className="paymentResult">
+            <table className="amount">
+              <tr>
+                <th>총 상품 금액</th>
+                <td>0원</td>
+              </tr>
+              <tr>
+                <th>배송비</th>
+                <td>+3000원</td>
+              </tr>
+            </table>
+            <table className="total">
+              <tr>
+                <th>결제예상금액</th>
+                <td>3000원</td>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
