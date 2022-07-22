7571be61b9de75cdb831817ddab06f1d569d1733
정예빈 <jeong-yebin@jeong-yebin-ui-MacBookAir.local>
2022-07-22 15:56:36 +0900

    슬라이드 기능 오류 수정

--- src/pages/Slides/Slides.js
+++ src/pages/Slides/Slides.js
@@ -1,12 +1,9 @@
-import { fireEvent } from '@testing-library/react';
 import React, { useEffect, useState } from 'react';
-import ImgList from './ImgList';
 import MainSlide from './MainSlide';
 import './Slides.scss';
 
 const Slides = () => {
   const [slideList, setSlideList] = useState([]);
-  const [currentSlide, setCurrentSlide] = useState(0);
   const [leftValue, setLeftValue] = useState(0);
   const copy = leftValue + 'vw';
 
@@ -18,23 +15,11 @@ const Slides = () => {
 
   // 이미지 슬라이드 기능 구현
   const prevSlide = () => {
-    if (currentSlide < slideList.length) {
-      setLeftValue(leftValue + 100);
-      // setCurrentSlide(currentSlide - 1);
-    }
+    leftValue === 0 ? setLeftValue(-200) : setLeftValue(leftValue + 100);
   };
 
-  // const nextSlide = () => {
-  //   if (currentSlide < slideList.length - 1) {
-  //     setLeftValue(leftValue - 100);
-  //     // setCurrentSlide(currentSlide + 1);
-  //   }
-  // };
-
   const nextSlide = () => {
-    currentSlide < slideList.length - 1
-      ? setCurrentSlide(leftValue - 100)
-      : setCurrentSlide(slideList.length + 1);
+    leftValue === -200 ? setLeftValue(0) : setLeftValue(leftValue - 100);
   };
 
   useEffect(() => {
