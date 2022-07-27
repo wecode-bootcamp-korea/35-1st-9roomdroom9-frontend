051053fda038774b5e0cd9c5f9374c80be71a323
정예빈 <jeong-yebin@jeong-yebin-ui-MacBookAir.local>
2022-07-27 14:50:39 +0900

    Modify : Main 레이아웃 및 기능구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,45 +1,55 @@
 import React, { useEffect, useState } from 'react';
-import NewList from './components/NewList';
-import BestList from './components/BestList';
-import GreenList from './components/GreenList';
+import ProductsList from './components/ProductsList';
 import Slides from '../Slides/Slides';
 import './Main.scss';
 
 const Main = () => {
-  const [mainList, setMainList] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('/data/main.json')
+  //     .then(res => res.json())
+  //     .then(result => setProducts(result));
+  // }, []);
 
   useEffect(() => {
-    fetch('http://10.58.0.44:8000/products/main')
+    fetch('http://10.58.0.83:8000/products/main')
       .then(res => res.json())
-      .then(result => setMainList(result));
+      .then(result => setProducts(result));
   }, []);
 
-  const { new_products, best_products, green_products } = mainList;
+  const { best_products, new_products, green_products } = products;
 
-  const isData = mainList.length !== 0;
-  if (!isData) return <>로딩중입니다....</>;
+  const isData = products.length !== 0;
 
-  return (
+  return !isData ? (
+    <div>로딩중입니다.</div>
+  ) : (
     <div className="main">
       <Slides />
       <div className="menu-wrap">
         <div className="best">
           <h2>요즘 잘 나가요</h2>
           <div className="best-wrap">
-            <BestList data={best_products} />
+            <ProductsList products={best_products} />
           </div>
         </div>
-        <div className="banner" />
+        <div className="banner">
+          <img src="/images/mainPage/banner-bg01.png" alt="배너1" />
+        </div>
         <div className="new">
           <h2>새로 나왔어요</h2>
           <div className="new-wrap">
-            <NewList data={new_products} />
+            <ProductsList products={new_products} />
           </div>
+        </div>
+        <div className="banner">
+          <img src="/images/mainPage/banner-bg02.png" alt="배너2" />
         </div>
         <div className="green">
           <h2>지금은 할인중</h2>
           <div className="green-wrap">
-            <GreenList data={green_products} />
+            <ProductsList products={green_products} />
           </div>
         </div>
       </div>
