6428482febd34a2093245e170f5a6722333b8a01
Parzival8971 <esh8971@gmail.com>
2022-10-20 02:39:18 +0900

    https to http

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://3.139.66.73:8000';
+export const BASE_URL = 'http://3.139.66.73:8000';
 
 // import { GET_PRODUCT_API } from '../../../config.js';
 // fetch(`${GET_PRODUCT_API}/5`).then(...).then(...);
