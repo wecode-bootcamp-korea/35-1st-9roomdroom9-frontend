f4fbb9f7a455185247387f665a39459b543249c1
Parzival8971 <esh8971@gmail.com>
2022-11-27 00:47:43 +0900

    axios추가하여 LoginForm에 적용한다

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://3.139.66.73:8000';
-// export const BASE_URL = 'http://43.201.66.208:8000';
+// export const BASE_URL = 'http://3.139.66.73:8000';
+export const BASE_URL = 'http://43.201.66.208:8000';
 
 // import { GET_PRODUCT_API } from '../../../config.js';
 // fetch(`${GET_PRODUCT_API}/5`).then(...).then(...);
--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { BASE_URL } from '../../config';
 import './LoginForm.scss';
@@ -6,34 +7,69 @@ import './LoginForm.scss';
 const LoginForm = () => {
   const navigate = useNavigate();
 
-  const postUserData = e => {
+  // async await axios로 변환과 에러 핸들링을 적용 해본다.
+  const postUserData = async e => {
     e.preventDefault();
-    fetch(`${BASE_URL}/users/login`, {
-      method: 'POST',
-      body: JSON.stringify({
-        email: userId,
-        password: userPw,
-      }),
-    })
-      .then(res => res.json())
-      .then(result => {
-        if (result.access_token) {
-          localStorage.setItem('token', result.access_token);
-          localStorage.setItem('name', result.name);
-          alert(`로그인을 환영합니다 ${userId}님`);
-          navigate('/');
-          window.location.reload();
-        } else if (result.message === 'INVALID_USER') {
-          alert(
-            '아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.'
-          );
-        } else if (result.message === 'User matching query does not exist.') {
-          alert(
-            '아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.'
-          );
-        }
-      });
+    try {
+      const response = await axios.post(
+        `${BASE_URL}/users/login`,
+        {
+          email: userId,
+          password: userPw,
+        },
+        { headers: { 'Content-Type': 'application/json' } }
+      );
+      if (response.data.access_token) {
+        localStorage.setItem('token', response.data.access_token);
+        localStorage.setItem('name', response.data.name);
+        alert(`로그인을 환영합니다 ${userId}님`);
+        navigate('/');
+        window.location.reload();
+      }
+    } catch (error) {
+      if (error.response.data.message === 'INVALID_USER') {
+        alert(
+          '아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.'
+        );
+      }
+      if (error.response.data.message === 'User_DoseNotExist') {
+        alert(
+          '아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.'
+        );
+      }
+      console.error('에러발생', error.response.data.message);
+    }
   };
+
+  // fetch에 대한 부분 학습용으로 저장
+  // const postUserData = e => {
+  //   e.preventDefault();
+  //   fetch(`${BASE_URL}/users/login`, {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       email: userId,
+  //       password: userPw,
+  //     }),
+  //   })
+  //     .then(res => res.json())
+  //     .then(result => {
+  //       if (result.access_token) {
+  //         localStorage.setItem('token', result.access_token);
+  //         localStorage.setItem('name', result.name);
+  //         alert(`로그인을 환영합니다 ${userId}님`);
+  //         navigate('/');
+  //         window.location.reload();
+  //       } else if (result.message === 'INVALID_USER') {
+  //         alert(
+  //           '아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.'
+  //         );
+  //       } else if (result.message === 'User matching query does not exist.') {
+  //         alert(
+  //           '아이디 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.'
+  //         );
+  //       }
+  //     });
+  // };
 
   const [inputValue, setInputValue] = useState({
     userId: '',
@@ -69,7 +105,7 @@ const LoginForm = () => {
                     type={input.type}
                     placeholder={input.placeholder}
                     autoFocus={input.autoFocus}
-                    autocomplete="off"
+                    autoComplete="off"
                   />
                   <p className="text-valid">{input.valid}</p>
                 </div>
